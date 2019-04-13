adc7fbe3fa85778e2614da74bff5a4e12de0076e
John Noble <jn@jnoble.net>
2019-04-13 18:06:04 +1000

    Initial commit

---
+++ doc/spec.md
@@ -0,0 +1,10 @@
+# When Is It On?
+
+Version 1.0 just does the next SpaceX launch.
+
+It will:
+
+1. Detect your computer's timezone.
+2. Tell you when the next SpaceX launch is, in that timezone.
+
+The 'next SpaceX launch' date is hard-coded by you in v1.0.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Header, List } from "semantic-ui-react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { records: null };
+  }
+
+  async componentWillMount() {
+    const key = "keyLiuEgDfEDAxUbf";
+    const url =
+      "https://api.airtable.com/v0/appouNZsxBNr6RBZ4/Events?api_key=keyLiuEgDfEDAxUbf";
+
+    const resp = await fetch(url);
+    const json = await resp.json();
+    const { records } = json;
+    this.setState({ records });
+    // console.log(records);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container text style={{ margin: "40px auto", "max-width": "650px" }}>
+        <Header as="h1">When Is It On?</Header>
+        {this.state.records ? (
+          <ul>
+            {this.state.records
+              .sort((a, b) => a.fields.UTC > b.fields.UTC)
+              .map(record => {
+                console.log(record.fields.UTC.toString());
+                const launchGMT = new Date(record.fields.UTC.toString());
+                return (
+                  <li style={{ padding: "5px" }}>
+                    {record.fields.Name}
+                    <ul>
+                      <li style={{ padding: "5px" }}>{launchGMT.toString()}</li>
+                    </ul>
+                  </li>
+                );
+              })}
+          </ul>
+        ) : null}
+      </Container>
     );
   }
 }
